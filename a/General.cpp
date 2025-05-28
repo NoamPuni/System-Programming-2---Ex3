119266d144dc0226a6285700e96e082bd4b5e23f
NoamPuni <noampuni99@gmail.com>
2025-05-28 11:47:28 +0300

    30

--- a/General.cpp
+++ a/General.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include "General.hpp"
 #include "Player.hpp"
 #include <stdexcept>
@@ -6,9 +5,6 @@
 #include <iostream>
 General::General(const std::string& name) : Player(name) {
     // Constructor initializes the General with a name
-}
-General::~General() {
-    // Destructor for General
 }
 void General::onArrestedBy(Player& attacker) {
     // General's specific behavior when arrested by another player
